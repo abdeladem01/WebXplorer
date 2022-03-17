36f14331ae9b931cff1a40313bc0cc70480eacd3
Abdeladem <dadem.fattah@gmail.com>
2022-03-17 21:28:05 +0100

    PROTOTYPE HOME PAGE

--- angular-webxplorer/src/app/title-page/title-page.component.ts
+++ angular-webxplorer/src/app/title-page/title-page.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TitlePageComponent implements OnInit {
   title = 'WebXploRer';
+  centered = false;
+  disabled = false;
+  unbounded = false;
+
+  radius=300;color="rgba(5,89,124,0.89)";
   constructor() { }
 
   ngOnInit(): void {
