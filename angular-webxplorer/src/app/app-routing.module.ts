2492d9abf3236127e7e0de918badfcc635271dc7
Abdeladem <dadem.fattah@gmail.com>
2022-04-08 02:47:12 +0200

    version intermediaire non fini de waiting page go dodo

--- angular-webxplorer/src/app/app-routing.module.ts
+++ angular-webxplorer/src/app/app-routing.module.ts
@@ -4,15 +4,17 @@ import {SigninSignupComponent} from "./signin-signup/signin-signup.component";
 import {TitlePageComponent} from "./title-page/title-page.component";
 import {GuestOrConnectComponent} from "./guest-or-connect/guest-or-connect.component";
 import {GameSettingsComponent} from "./game-settings/game-settings.component";
+import {WaitingPageComponent} from "./waiting-page/waiting-page.component";
 
 const routes: Routes = [
   {path: 'sign', component: SigninSignupComponent},
   {path: 'home', component: TitlePageComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   {path: 'playas', component: GuestOrConnectComponent},
-  {path: 'gamesettings', component: GameSettingsComponent}
+  {path: 'gamesettings', component: GameSettingsComponent},
+  {path: 'waitingpage', component: WaitingPageComponent}
 ];
- 
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
