e55563d452fbb95d1772f6437fdbbc73dc17a7b1
GitMed22 <96897244+GitMed22@users.noreply.github.com>
2022-05-20 00:22:45 +0200

    Update News.ts

--- angular-webxplorer/src/mocks/News.ts
+++ angular-webxplorer/src/mocks/News.ts
@@ -1,5 +1,194 @@
 import {Screen} from "../models/screen.model";
 
 export const SCREEN_LIST: Screen[] = [
+  
+  {
+    url: 'https://imgur.com/WlNqaRc' ,
+    date: 20010626,
+  },
+  {
+    url: 'https://imgur.com/9Rmpxm1' ,
+    date:,
+  },
+  {
+    url:'https://imgur.com/helqxcm' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/1S87F8e' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/IFneIcx' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/FtJNEwm' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/LxV9AIq' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/PkqMqsD' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/Ad1Bg8l' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/RlbQRDr' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/avgKjhL' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/gZPKv4t' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/Chadbag' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/oB8ipoW' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/1tu6i75' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/0cZYwVw' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/sbKTry7' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/TUWYfBD' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/pJsgX9J' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/NWv5Mg9' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/TQBAIkS' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/Xm5HTBr' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/Pv7cr3m' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/ugaJN6G' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/R2WIHm4' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/A3gTDmo' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/W2V1dMr' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/OhdxXTD' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/vLgeU3F' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/u6mM1yG' ,
+    date:,
+  },
+  {
+    url: 'https://imgur.com/cg408qR' ,
+    date:,
+  },
+  {
+    url: '' ,
+    date:,
+  },
+  {
+    url: '' ,
+    date:,
+  },
+  {
+    url: '' ,
+    date:,
+  },
+  {
+    url: '' ,
+    date:,
+  },
+  {
+    url: '' ,
+    date:,
+  },
+  {
+    url: '' ,
+    date:,
+  },
+  {
+    url: '' ,
+    date:,
+  },
+  {
+    url: '' ,
+    date:,
+  },
+  {
+    url: '' ,
+    date:,
+  },
+  {
+    url: '' ,
+    date:,
+  },
+  {
+    url: '' ,
+    date:,
+  },
+  {
+    url: '' ,
+    date:,
+  },
+  {
+    url: '' ,
+    date:,
+  },
+  {
+    url: '' ,
+    date:,
+  },
+  {
+    url: '' ,
+    date:,
+  },
+
+
+
+
 
 ]; 
