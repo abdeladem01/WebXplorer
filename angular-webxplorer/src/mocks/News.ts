5b403b1bfa6df86822906a548d0b9a6352aa0b94
GitMed22 <96897244+GitMed22@users.noreply.github.com>
2022-05-20 00:44:32 +0200

    Update News.ts

--- angular-webxplorer/src/mocks/News.ts
+++ angular-webxplorer/src/mocks/News.ts
@@ -3,127 +3,127 @@ import {Screen} from "../models/screen.model";
 export const SCREEN_LIST: Screen[] = [
   
   {
-    url: 'https://imgur.com/WlNqaRc' ,
+    url: 'https://i.imgur.com/WlNqaRc.png' ,
     date: 20010626,
   },
   {
-    url: 'https://imgur.com/9Rmpxm1' ,
+    url: 'https://i.imgur.com/9Rmpxm1.png' ,
     date: 20010913,
   },
   {
-    url:'https://imgur.com/helqxcm' ,
+    url: 'https://i.imgur.com/helqxcm.png' ,
     date: 20020123,
   },
   {
-    url: 'https://imgur.com/1S87F8e' ,
+    url: 'https://i.imgur.com/1S87F8e.png' ,
     date: 20020928,
   },
   {
-    url: 'https://imgur.com/IFneIcx' ,
+    url: 'https://i.imgur.com/IFneIcx.png' ,
     date: 20030202,
   },
   {
-    url: 'https://imgur.com/FtJNEwm' ,
+    url: 'https://i.imgur.com/FtJNEwm.png' ,
     date: 20030727,
   },
   {
-    url: 'https://imgur.com/LxV9AIq' ,
+    url: 'https://i.imgur.com/LxV9AIq.png' ,
     date: 20040609,
   },
   {
-    url: 'https://imgur.com/PkqMqsD' ,
+    url: 'https://i.imgur.com/PkqMqsD.png' ,
     date: 20041229,
   },
   {
-    url: 'https://imgur.com/Ad1Bg8l' ,
+    url: 'https://i.imgur.com/Ad1Bg8l.png' ,
     date: 20050118,
   },
   {
-    url: 'https://imgur.com/RlbQRDr' ,
+    url: 'https://i.imgur.com/RlbQRDr.png' ,
     date: 20050923,
   },
   {
-    url: 'https://imgur.com/avgKjhL' ,
+    url: 'https://i.imgur.com/avgKjhL.png' ,
     date: 20060102,
   },
   {
-    url: 'https://imgur.com/gZPKv4t' ,
+    url: 'https://i.imgur.com/gZPKv4t.png' ,
     date: 20060614,
   },
   {
-    url: 'https://imgur.com/Chadbag' ,
+    url: 'https://i.imgur.com/Chadbag.png' ,
     date: 20070329,
   },
   {
-    url: 'https://imgur.com/oB8ipoW' ,
+    url: 'https://i.imgur.com/oB8ipoW.png' ,
     date: 20071008,
   },
   {
-    url: 'https://imgur.com/1tu6i75' ,
+    url: 'https://i.imgur.com/1tu6i75.png' ,
     date: 20080131,
   },
   {
-    url: 'https://imgur.com/0cZYwVw' ,
+    url: 'https://i.imgur.com/0cZYwVw.png' ,
     date: 20081106,
   },
   {
-    url: 'https://imgur.com/sbKTry7' ,
+    url: 'https://i.imgur.com/sbKTry7.png' ,
     date: 20090216,
   },
   {
-    url: 'https://imgur.com/TUWYfBD' ,
+    url: 'https://i.imgur.com/TUWYfBD.png' ,
     date: 20100417,
   },
   {
-    url: 'https://imgur.com/pJsgX9J' ,
+    url: 'https://i.imgur.com/pJsgX9J.png' ,
     date: 20101016,
   },
   {
-    url: 'https://imgur.com/NWv5Mg9' ,
+    url: 'https://i.imgur.com/NWv5Mg9.png' ,
     date: 20110203,
   },
   {
-    url: 'https://imgur.com/TQBAIkS' ,
+    url: 'https://i.imgur.com/TQBAIkS.png' ,
     date: 20110721,
   },
   {
-    url: 'https://imgur.com/Xm5HTBr' ,
+    url: 'https://i.imgur.com/Xm5HTBr.png' ,
     date: 20121204,
   },
   {
-    url: 'https://imgur.com/Pv7cr3m' ,
+    url: 'https://i.imgur.com/Pv7cr3m.png' ,
     date: 20131228,
   },
   {
-    url: 'https://imgur.com/ugaJN6G' ,
+    url: 'https://i.imgur.com/ugaJN6G.png' ,
     date: 20140105,
   },
   {
-    url: 'https://imgur.com/R2WIHm4' ,
+    url: 'https://i.imgur.com/R2WIHm4.png' ,
     date: 20151120,
   },
   {
-    url: 'https://imgur.com/A3gTDmo' ,
+    url: 'https://i.imgur.com/A3gTDmo.png' ,
     date: 20160813,
   },
   {
-    url: 'https://imgur.com/W2V1dMr' ,
+    url: 'https://i.imgur.com/W2V1dMr.png' ,
     date: 20170402,
   },
   {
-    url: 'https://imgur.com/OhdxXTD' ,
+    url: 'https://i.imgur.com/OhdxXTD.png' ,
     date: 20181012,
   },
   {
-    url: 'https://imgur.com/vLgeU3F' ,
+    url: 'https://i.imgur.com/vLgeU3F.png' ,
     date: 20190206,
   },
   {
-    url: 'https://imgur.com/u6mM1yG' ,
+    url: 'https://i.imgur.com/u6mM1yG.png' ,
     date: 20201109,
   },
   {
-    url: 'https://imgur.com/cg408qR' ,
+    url: 'https://i.imgur.com/cg408qR.png' ,
     date: 20210410,
   },
   {
